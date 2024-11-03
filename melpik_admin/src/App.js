59b02fc4a8aa9a1f85f6758e450041f7a3f15901
youminki <dbalsrl7648@gmail.com>
2024-11-03 16:31:26 +0900

    feat : lazy 설정

--- melpik_admin/src/App.js
+++ melpik_admin/src/App.js
@@ -1,27 +1,50 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
+import styled from 'styled-components';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import Theme from './styles/Theme';
-import AdminLogin from './pages/AdminLogin';
+
+// Lazy-load the AdminLogin component
+const AdminLogin = React.lazy(() => import('./pages/AdminLogin'));
 
 function App() {
   return (
-    <Router>
-      <ThemeProvider theme={Theme}>
+    <ThemeProvider theme={Theme}>
+      <Router>
         <GlobalStyle />
         <div className='App'>
           <header className='App-header'>
-            <Routes>
-              <Route path='/admin/auth/login' element={<AdminLogin />} />
-            </Routes>
+            <Suspense fallback={<LoadingMessage>로딩 중...</LoadingMessage>}>
+              <Routes>
+                {/* Redirect from '/' to '/admin/auth/login' */}
+                <Route
+                  path='/'
+                  element={<Navigate to='/admin/auth/login' replace />}
+                />
+                <Route path='/admin/auth/login' element={<AdminLogin />} />
+              </Routes>
+            </Suspense>
           </header>
-          ㄴ
         </div>
-      </ThemeProvider>
-    </Router>
+      </Router>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
+// Style for lazy-loading fallback
+const LoadingMessage = styled.div`
+  color: yellow; /* 노란색으로 스타일링 */
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+  margin-top: 20px;
+`;
